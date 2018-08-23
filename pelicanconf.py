5f753450e886ef74987c4f769b6a2fd82b788977
shikajiro <shikajiro@gmail.com>
2018-08-24 08:36:34 +0900

    themeのdirを修正

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,7 +41,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "/Users/shikajiro/Documents/pelican-themes/Flex"
+THEME = "/Users/shikajiro/Develop/pelican-themes/Flex"
 
 # PLUGIN_PATHS = ["/Users/shikajiro/Documents/pelican-plugins"]
 PLUGINS = ["pelican_gist"]
